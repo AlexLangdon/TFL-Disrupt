b8113015e6d823355cfc296876479f0ab583f54e
AlexLangdon <a.a.langdon.4@gmail.com>
2016-10-29 18:59:27 +0100

    Trying to get response from TFL.

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+import requests
+
 app = Flask(__name__)
+apiKeyCode = {}
 
 @app.route('/')
 def hello_world():
-    return 'Hello World! STUFF'
+    res = requests.get('https://api.tfl.gov.uk/Line/Mode/tube/Status?detail=False')
+    return 'Hello World!',res.text
 
 if __name__ == '__main__':
     app.run()
