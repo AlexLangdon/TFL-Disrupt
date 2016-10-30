8ed70aea1715c86d02767851f5e3c90fabee1b71
JordanL1 <jordan.lees1@gmail.com>
2016-10-30 08:44:38 +0000

    Forgot to dump json in get_line

--- sms_in.py
+++ sms_in.py
@@ -1,6 +1,5 @@
 from twilio.rest import TwilioRestClient
 from flask import Flask, request
-from query_tfl import query_tfl_obj
 import apiai
 import twillio_tokens
 import json
@@ -13,7 +12,8 @@ API_AI_CLIENT_TOKEN = "4eb996e7858f44bcb2c6242d112e8517"
 ai = apiai.ApiAI(API_AI_CLIENT_TOKEN)
 
 def get_line(self, apiai_json) :
-    line = apiai_json["result"]["parameters"]["line"]
+    line_py_obj = json.dumps(apiai_json)
+    line = line_py_obj["result"]["parameters"]["line"]
     return line
 
 def sms_in() :
